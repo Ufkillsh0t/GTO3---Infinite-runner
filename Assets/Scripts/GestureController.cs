78bcab36ea045e0f5a4b44abf83e8c3ecefbe919
Tim Goes <Tim_Goes4@hotmail.nl>
2016-09-08 12:28:05 +0200

    Inital commit with own Unity Gesture Controller

---
+++ Assets/Scripts/GestureController.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Gestures
+{
+    None,
+    Touch,
+    SwipeUp,
+    SwipeUpperLeft,
+    SwipeUpperRight,
+    SwipeDown,
+    SwipeDownLeft,
+    SwipeDownRight,
+    SwipeLeft,
+    SwipeRight,
+    TwoFingerSwipeInwards,
+    TwoFingerSwipeOutwards
+}
+
+public class GestureController : MonoBehaviour
+{
+    public Gestures lastGesture = Gestures.None;
+    public Gestures currentGesture = Gestures.None;
+    public float minimalSwipeDistanceY = 0.10f;
+    public float minimalSwipeDistanceX = 0.10f;
+
+    public Vector2 startPosition;
+    public Vector2 endPosition;
+    //public Vector2 startPosition2;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.touches[0];
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startPosition = touch.position;
+                    break;
+                case TouchPhase.Ended:
+                    lastGesture = currentGesture;
+                    currentGesture = SetTouchGesture(touch);
+                    break;
+            }
+        }
+        else
+        {
+            if (currentGesture != Gestures.None) lastGesture = currentGesture;
+            currentGesture = Gestures.None;
+        }
+    }
+
+    public Gestures SetTouchGesture(Touch touch)
+    {
+        endPosition = touch.position;
+
+        float swipeValueY = touch.position.y - startPosition.y;
+        float swipeDistanceY = (new Vector2(0, startPosition.y) - new Vector2(0, touch.position.y)).magnitude;
+
+        float swipeValueX = touch.position.x - startPosition.x;
+        float swipeDistanceX = (new Vector2(0, startPosition.x) - new Vector2(0, touch.position.x)).magnitude;
+
+        Debug.Log("SwipeValueY:" + swipeValueY + " SwipeDistY:" + swipeDistanceY + " SwipevalueX:" + swipeValueX + " SwipeDistX:" + swipeDistanceX);
+
+        if (swipeDistanceX > minimalSwipeDistanceX)
+        {
+            if (swipeDistanceY > minimalSwipeDistanceY)
+            {
+                return GetDiagonalSwipe(swipeValueX, swipeValueY);
+            }
+            else
+            {
+                return GetHorizontalSwipe(swipeValueX);
+            }
+        }
+        else if (swipeDistanceY > minimalSwipeDistanceY)
+        {
+            return GetVerticalSwipe(swipeValueY);
+        }
+        else
+        {
+            return Gestures.Touch;
+        }
+    }
+
+    private Gestures GetDiagonalSwipe(float swipeValueX, float swipeValueY)
+    {
+        if (swipeValueX > 0 && swipeValueY > 0)
+        {
+            return Gestures.SwipeUpperRight;
+        }
+        else if (swipeValueX > 0 && swipeValueY < 0)
+        {
+            return Gestures.SwipeDownRight;
+        }
+        else if (swipeValueX < 0 && swipeValueY < 0)
+        {
+            return Gestures.SwipeDownLeft;
+        }
+        else
+        {
+            return Gestures.SwipeUpperLeft;
+        }
+    }
+
+    private Gestures GetHorizontalSwipe(float swipeValueX)
+    {
+        if (swipeValueX > 0)
+        {
+            return Gestures.SwipeRight;
+        }
+        else
+        {
+            return Gestures.SwipeLeft;
+        }
+    }
+
+    private Gestures GetVerticalSwipe(float swipeValueY)
+    {
+        if (swipeValueY > 0)
+        {
+            return Gestures.SwipeUp;
+        }
+        else
+        {
+            return Gestures.SwipeDown;
+        }
+    }
+}
+//int touches = Input.touchCount > 2 ? 2 : Input.touchCount;
+//Gestures
+//for(int i = 0; i < touches)
+//{
+//    switch
+//}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+    public bool stream_mobile_debug = true;
+    public float xSpeed = 10f;
+    public float zSpeed = 10f;
+    public float mobileSpeedMultiplier = 0.25f;
+    public float jumpSpeed = 10f;
+    public float deathPosY = -6f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Input.acceleration.x * Time.deltaTime, 0, Input.acceleration.z * Time.deltaTime);
+
+        if (SystemInfo.deviceType == DeviceType.Desktop && !stream_mobile_debug)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.Translate(xSpeed * horizontal * Time.deltaTime, jumpSpeed * Time.deltaTime, zSpeed * vertical * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(xSpeed * horizontal * Time.deltaTime, 0, zSpeed * vertical * Time.deltaTime);
+            }
+        }
+        else if (SystemInfo.deviceType != DeviceType.Console || stream_mobile_debug)
+        {
+            if (Input.touchCount > 0)
+            {
+                transform.Translate(Input.acceleration.x * xSpeed * mobileSpeedMultiplier * Time.deltaTime,
+                                    jumpSpeed * Time.deltaTime,
+                                    -Input.acceleration.z * zSpeed * mobileSpeedMultiplier * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Input.acceleration.x * xSpeed * mobileSpeedMultiplier * Time.deltaTime, 0, -Input.acceleration.z * zSpeed * mobileSpeedMultiplier * Time.deltaTime);
+            }
+        }
+
+        if (transform.position.y < deathPosY)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
