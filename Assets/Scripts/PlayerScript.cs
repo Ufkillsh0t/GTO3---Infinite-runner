cbf029d3a13423bb0bf8049b313a3f846a6414ad
Tim Goes <Tim_Goes4@hotmail.nl>
2016-09-15 09:17:04 +0200

    Gesture controller en playerscript gelinkt

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,11 +5,21 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour
 {
     public bool stream_mobile_debug = true;
+    public bool moveAccelerometer = false;
     public float xSpeed = 10f;
     public float zSpeed = 10f;
     public float mobileSpeedMultiplier = 0.25f;
+    public float swipeSpeed = 1.5f;
+
     public float jumpSpeed = 10f;
     public float deathPosY = -6f;
+
+    private GestureController controller;
+
+    void Awake()
+    {
+        controller = gameObject.GetComponent<GestureController>();
+    }
 
     // Use this for initialization
     void Start()
@@ -38,7 +48,32 @@ public class PlayerScript : MonoBehaviour
         }
         else if (SystemInfo.deviceType != DeviceType.Console || stream_mobile_debug)
         {
-            if (Input.touchCount > 0)
+            MobileInput();
+        }
+
+        if (transform.position.y < deathPosY)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void MobileInput()
+    {
+        if (moveAccelerometer)
+        {
+            AccelerometerControles();
+        }
+        else
+        {
+            GestureControles();
+        }
+    }
+
+    private void AccelerometerControles()
+    {
+        if (controller != null)
+        {
+            if (controller.currentGesture == Gestures.Touch)
             {
                 transform.Translate(Input.acceleration.x * xSpeed * mobileSpeedMultiplier * Time.deltaTime,
                                     jumpSpeed * Time.deltaTime,
@@ -49,10 +84,97 @@ public class PlayerScript : MonoBehaviour
                 transform.Translate(Input.acceleration.x * xSpeed * mobileSpeedMultiplier * Time.deltaTime, 0, -Input.acceleration.z * zSpeed * mobileSpeedMultiplier * Time.deltaTime);
             }
         }
-
-        if (transform.position.y < deathPosY)
+        else
         {
-            SceneManager.LoadScene(0);
+            Debug.LogError("Gesture controller not found");
         }
+    }
+
+    private void GestureControles()
+    {
+        if (controller != null)
+        {
+            if (controller.currentGesture != Gestures.None)
+            {
+                switch (controller.currentGesture)
+                {
+                    case Gestures.Touch:
+                        UseItem();
+                        break;
+                    case Gestures.SwipeDown:
+                        Slide();
+                        break;
+                    case Gestures.SwipeDownLeft:
+                        Slide();
+                        MoveLeft();
+                        break;
+                    case Gestures.SwipeDownRight:
+                        Slide();
+                        MoveRight();
+                        break;
+                    case Gestures.SwipeLeft:
+                        MoveLeft();
+                        break;
+                    case Gestures.SwipeRight:
+                        MoveRight();
+                        break;
+                    case Gestures.SwipeUp:
+                        Jump();
+                        break;
+                    case Gestures.SwipeUpperLeft:
+                        Jump();
+                        MoveLeft();
+                        break;
+                    case Gestures.SwipeUpperRight:
+                        Jump();
+                        MoveRight();
+                        break;
+                    case Gestures.TwoFingerSwipeOutwards:
+                        Debug.Log("Outwards swipe");
+                        break;
+                    case Gestures.TwoFingerTouch:
+                        Debug.Log("Two finger touch");
+                        break;
+                }
+            }
+            Move();
+        }
+        else
+        {
+            Debug.LogError("Gesture controller not found");
+        }
+    }
+
+    private void UseItem()
+    {
+        Debug.Log("Used an item");
+    }
+
+    private void Jump()
+    {
+        transform.Translate(0, jumpSpeed, 0);
+        Debug.Log("Jump");
+    }
+
+    private void Slide()
+    {
+        Debug.Log("Slide");
+    }
+
+    private void MoveLeft()
+    {
+        transform.Translate(-swipeSpeed, 0f, 0f);
+        Debug.Log("Moved to the left");
+    }
+
+    private void MoveRight()
+    {
+        transform.Translate(swipeSpeed, 0f, 0f);
+        Debug.Log("Moved to the right");
+    }
+
+    private void Move()
+    {
+        transform.Translate(0f, 0f, xSpeed * Time.deltaTime);
     }
 }
