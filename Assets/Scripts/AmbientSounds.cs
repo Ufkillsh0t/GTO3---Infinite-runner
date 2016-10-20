e13ce559182a05ebde15336fcbd8c73b26278d42
Tim goes <Tim_Goes4@hotmail.nl>
2016-10-21 01:41:54 +0200

    ambientsound volume paramater gebeuren

--- Assets/Scripts/AmbientSounds.cs
+++ Assets/Scripts/AmbientSounds.cs
@@ -1,17 +1,49 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AmbientSounds : MonoBehaviour {
+public class AmbientSounds : MonoBehaviour
+{
 
     private static PlayerScript player;
 
+    //Ambient sounds
+    public AudioClip[] ambientSounds;
+
+    //AudioSource used for ambientSounds;
+    private AudioSource ambient;
+
+    public int ambientChance = 500;
+
+
     // Use this for initialization
-    void Awake () {
+    void Awake()
+    {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    void Start()
+    {
+        ambient = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ambient.isPlaying)
+        {
+            if (Random.Range(0, ambientChance) == 0) PlayRandomAmbientSound();
+        }
+    }
+
+    private void PlayRandomAmbientSound()
+    {
+        if (ambient != null && ambientSounds != null && ambientSounds.Length > 0)
+        {
+            ambient.PlayOneShot(ambientSounds[Random.Range(0, ambientSounds.Length)]);
+        }
+        else
+        {
+            Debug.Log("The audiosource could not be found or there are no ambientsound avaible!");
+        }
+    }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,7 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Audio;
 
 public class GameController : MonoBehaviour {
-    
+
+    public AudioMixer masterMixer;
+
+
+    public float musicVolume = 80f;
+    private float currentMusicVolume;
+
+
+
+    public void SetMusicLvl(float musicLvl)
+    {
+        masterMixer.SetFloat("ambientVolume", musicLvl);
+    }
+
 
 }
