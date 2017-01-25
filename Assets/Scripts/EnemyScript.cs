ec7eb7b79399ca171dce757f708c0ec9e4085b71
Tim goes <Tim_Goes4@hotmail.nl>
2017-01-25 07:20:41 +0100

    Laatste commit

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -10,13 +10,14 @@ public class EnemyScript : MonoBehaviour
     public int defaultDistance = 5;
     public float enemySpeedPlayer = 0.9f;
     public float enemySpeed = 2f;
+    public float yPos = -0.5f;
 
     // Use this for initialization
     void Start()
     {
         if (player == null) player = gameObject.GetComponentInParent<PlayerScript>();
         if (pss == null) pss = gameObject.GetComponentInParent<PlayerSoundScript>();
-        transform.localPosition = new Vector3(0, 0, -defaultDistance);
+        transform.localPosition = new Vector3(0, yPos, -defaultDistance);
     }
 
     // Update is called once per frame
@@ -36,14 +37,14 @@ public class EnemyScript : MonoBehaviour
 
     public void ResetPosition()
     {
-        transform.localPosition = new Vector3(0, 0, -defaultDistance);
+        transform.localPosition = new Vector3(0, yPos, -defaultDistance);
     }
 
     private void Move()
     {
         if (player.currentSpeedMultiplier < enemySpeedPlayer)
         {
-            transform.localPosition = new Vector3(0, 0,
+            transform.localPosition = new Vector3(0, yPos,
                     transform.localPosition.z + (enemySpeedPlayer - player.currentSpeedMultiplier) * Time.deltaTime * enemySpeed);
         }
         if (player.currentSpeedMultiplier > enemySpeedPlayer)
@@ -55,11 +56,11 @@ public class EnemyScript : MonoBehaviour
             if ((transform.localPosition.z - distanceIncrease) >= -defaultDistance)
             {
                 float distance = transform.localPosition.z - distanceIncrease;
-                transform.localPosition = new Vector3(0, 0, distance);
+                transform.localPosition = new Vector3(0, yPos, distance);
             }
             else
             {
-                transform.localPosition = new Vector3(0, 0, -defaultDistance);
+                transform.localPosition = new Vector3(0, yPos, -defaultDistance);
             }
         }
     }
